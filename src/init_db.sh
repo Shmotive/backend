1ebd0f784a3399887ff08666a04c107d8f89651d
Ian Gu <iangu@Ians-MacBook-Pro.local>
2024-02-03 23:15:15 -0500

    feat: reset db and seed on docker up

--- src/init_db.sh
+++ src/init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # npm run migrate-db
-npx prisma db push
+npx prisma migrate reset
 npm run start
--- src/prisma/migrations/0_init/migration.sql
+++ src/prisma/migrations/0_init/migration.sql
@@ -33,8 +33,8 @@ CREATE TABLE "Recommendation" (
     "category" "RecommendationCategory" NOT NULL,
     "price_range_low" DOUBLE PRECISION,
     "price_range_high" DOUBLE PRECISION,
-    "suggested_by_uuid" TEXT NOT NULL,
-    "lobby_id" INTEGER NOT NULL,
+    "suggested_by_uuid" TEXT,
+    "custom_lobby_id" INTEGER,
 
     CONSTRAINT "Recommendation_pkey" PRIMARY KEY ("id")
 );
@@ -54,7 +54,7 @@ CREATE TABLE "Vote" (
     "yes_vote" BOOLEAN NOT NULL DEFAULT true,
     "recommendation_id" TEXT NOT NULL,
 
-    CONSTRAINT "Vote_pkey" PRIMARY KEY ("lobby_id","user_uuid","recommendation_id")
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("user_uuid","lobby_id","recommendation_id")
 );
 
 -- CreateTable
@@ -63,8 +63,17 @@ CREATE TABLE "_lobby_participant" (
     "B" TEXT NOT NULL
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "Vote_user_uuid_key" ON "Vote"("user_uuid");
+-- CreateTable
+CREATE TABLE "_generated_recommendations" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_lobby_picks" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL
+);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_lobby_participant_AB_unique" ON "_lobby_participant"("A", "B");
@@ -72,14 +81,26 @@ CREATE UNIQUE INDEX "_lobby_participant_AB_unique" ON "_lobby_participant"("A", 
 -- CreateIndex
 CREATE INDEX "_lobby_participant_B_index" ON "_lobby_participant"("B");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "_generated_recommendations_AB_unique" ON "_generated_recommendations"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_generated_recommendations_B_index" ON "_generated_recommendations"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_lobby_picks_AB_unique" ON "_lobby_picks"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_lobby_picks_B_index" ON "_lobby_picks"("B");
+
 -- AddForeignKey
 ALTER TABLE "Lobby" ADD CONSTRAINT "Lobby_owner_uuid_fkey" FOREIGN KEY ("owner_uuid") REFERENCES "User"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Recommendation" ADD CONSTRAINT "Recommendation_suggested_by_uuid_fkey" FOREIGN KEY ("suggested_by_uuid") REFERENCES "User"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Recommendation" ADD CONSTRAINT "Recommendation_suggested_by_uuid_fkey" FOREIGN KEY ("suggested_by_uuid") REFERENCES "User"("uuid") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Recommendation" ADD CONSTRAINT "Recommendation_lobby_id_fkey" FOREIGN KEY ("lobby_id") REFERENCES "Lobby"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Recommendation" ADD CONSTRAINT "Recommendation_custom_lobby_id_fkey" FOREIGN KEY ("custom_lobby_id") REFERENCES "Lobby"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "SkipRecommendation" ADD CONSTRAINT "SkipRecommendation_lobby_id_fkey" FOREIGN KEY ("lobby_id") REFERENCES "Lobby"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -101,4 +122,16 @@ ALTER TABLE "_lobby_participant" ADD CONSTRAINT "_lobby_participant_A_fkey" FORE
 
 -- AddForeignKey
 ALTER TABLE "_lobby_participant" ADD CONSTRAINT "_lobby_participant_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_generated_recommendations" ADD CONSTRAINT "_generated_recommendations_A_fkey" FOREIGN KEY ("A") REFERENCES "Lobby"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_generated_recommendations" ADD CONSTRAINT "_generated_recommendations_B_fkey" FOREIGN KEY ("B") REFERENCES "Recommendation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_lobby_picks" ADD CONSTRAINT "_lobby_picks_A_fkey" FOREIGN KEY ("A") REFERENCES "Lobby"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_lobby_picks" ADD CONSTRAINT "_lobby_picks_B_fkey" FOREIGN KEY ("B") REFERENCES "Recommendation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
